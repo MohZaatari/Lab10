b5d997a4bc14c2ddf17507f3118b4ddb1736902b
Mohamad ElZaatari <mzaatari@umich.edu>
2021-12-07 19:41:28 -0500

    Made testMixed and testDuplicates

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -60,42 +60,42 @@ testDuplicates();
 
     public void testMixed(){
 
+      int[] arr = new int[5];
+      arr[0] = 8;
+      arr[1] = -9;
+      arr[2] = -7;
+      arr[3] = 10;
+      arr[4] = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      int[] Sortedarr = new int[5];
+      Sortedarr[0] = -9;
+      Sortedarr[1] = -7;
+      Sortedarr[2] = 0;
+      Sortedarr[3] = 8;
+      Sortedarr[4] = 10;
+      SelectionSort temp1 = new SelectionSort();
+      temp1.basicSelectionSort(arr);
+      assertArrayEquals("Not Equal",Sortedarr,arr);
     }
 
     public void testDuplicates(){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       
+      int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = 8;
+        arr[2] = -7;
+        arr[3] = -7;
+        arr[4] = 2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -7;
+        Sortedarr[1] = -7;
+        Sortedarr[2] = 2;
+        Sortedarr[3] = 8;
+        Sortedarr[4] = 8;
+
+        SelectionSort temp1 = new SelectionSort();
+        temp1.basicSelectionSort(arr);
+        assertArrayEquals("Not Equal",Sortedarr,arr);
     }
 }
